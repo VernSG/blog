e91af85e1838480fc87b04440efaaeee213e535e
VernSG <snexmania76@gmail.com>
2024-10-09 01:53:19 +0800

    changes(about): add about umkt ti blog
    Signed-off-by: VernSG <snexmania76@gmail.com>

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,32 +7,15 @@ const About: NextPage = () => {
     <>
       <Page
         title="About"
-        description="TIni adalah tentang Blog ini."
+        description="Ini adalah tentang Blog ini."
       >
         <Prose>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sed
-            aliquam tempora nihil magni natus mollitia. Doloribus,
-            exercitationem iusto odio asperiores dolor, alias excepturi maxime
-            distinctio quod eum saepe eos!
+          Haloo Sobat Mahasiswaa, Selamat datang di Blog UMKT! Kami adalah komunitas mahasiswa yang berkomitmen untuk berbagi informasi, pengalaman, dan inspirasi seputar dunia kampus dan kehidupan mahasiswa.
+          Di sini, kamu akan menemukan berbagai artikel yang membahas topik-topik menarik, mulai dari tips akademik, cerita pengalaman pribadi, hingga panduan pengembangan diri yang relevan dengan kehidupan kampus.
           </p>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sed
-            aliquam tempora nihil magni natus mollitia. Doloribus,
-            exercitationem iusto odio asperiores dolor, alias excepturi maxime
-            distinctio quod eum saepe eos!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sed
-            aliquam tempora nihil magni natus mollitia. Doloribus,
-            exercitationem iusto odio asperiores dolor, alias excepturi maxime
-            distinctio quod eum saepe eos!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sed
-            aliquam tempora nihil magni natus mollitia. Doloribus,
-            exercitationem iusto odio asperiores dolor, alias excepturi maxime
-            distinctio quod eum saepe eos!
+          Blog UMKT ini didedikasikan untuk menjadi wadah bagi mahasiswa untuk saling berbagi pengetahuan, bertukar pikiran, dan memperluas wawasan. Kami percaya bahwa dengan berbagi, kita bisa belajar lebih banyak dan tumbuh bersama.
           </p>
         </Prose>
       </Page>
